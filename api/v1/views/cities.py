c71335292a752c0f50a24baaae72cb0adffb0bb2
Abdulbasit <aabdulrafiu721@gmail.com>
2023-08-12 10:33:21 +0100

    fixed typo

--- api/v1/views/cities.py
+++ api/v1/views/cities.py
@@ -37,6 +37,7 @@ def get_state_with_id(state_id=None):
 
     if request.method == 'DELETE':
         state_obj.delete()
+        del state_obj
         return jsonify({}), 200
 
     if request.method == 'PUT':
