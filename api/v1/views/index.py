0e17ef5fcc5427dc07986988244e8410e3df9a61
Abdulbasit <aabdulrafiu721@gmail.com>
2023-08-12 08:18:04 +0100

    added endpoint for stats

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -11,3 +11,21 @@ def status():
     if request.method == 'GET':
         resp = {"status": "OK"}
         return jsonify(resp)
+
+
+@app_views.route('/stats', methods=['GET'])
+def stats():
+    """returns the numbers of each objects by type"""
+    if request.method == 'GET':
+        response = {}
+        OBJECTS = {
+             "Amenity": "amenities",
+             "City": "cities",
+             "Place": "places",
+             "Review": "reviews",
+             "State": "states",
+             "User": "users"
+          }
+        for key, value in OBJECTS.items():
+            response[value] = storage.count(key)
+        return jsonify(response)
